e696f574ffbd1ac8d5eaac9d78466da5405983f6
48m13s <56842015+48m13s@users.noreply.github.com>
2020-01-15 16:44:55 +0300

    Parallax v.3

--- Assets/Scripts/Parallax2.cs
+++ Assets/Scripts/Parallax2.cs
@@ -2,27 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+public class Parallax2 : MonoBehaviour
 {
-	private float length, startposX, startposY;
+	private float length, startposX;
 	public GameObject cam;
 	public float parallaxEffectX;
-	public float parallaxEffectY;
     // Start is called before the first frame update
     void Start()
     {
         startposX = transform.position.x;
 	length = GetComponent<SpriteRenderer>().bounds.size.x;
-	startposY = transform.position.y;
-	length = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float distX = (cam.transform.position.x * parallaxEffectX);
-	float distY = (cam.transform.position.y * parallaxEffectY);
 
-	transform.position = new Vector3(startposX + distX, startposY + distY, transform.position.z);
+	transform.position = new Vector3(startposX + distX, transform.position.z, transform.position.z);
     }
 }
