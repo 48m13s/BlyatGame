1beec4706997705b14b9b4754a7c9aad2c7fcfc6
48m13s <56842015+48m13s@users.noreply.github.com>
2020-01-15 15:49:17 +0300

    Parallax v.2

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -15,7 +15,7 @@ public class Parallax : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float dist = (cam.transform.position.x * parallaxEffect);
 
