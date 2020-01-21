1307eb412fe0dec4f567c41f6921fcfa5e025881
48m13s <48m13s@gmail.com>
2020-01-21 20:24:25 +0300

    sjdfhjh

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -5,33 +5,21 @@ using System.Collections.Generic;
 public class CharacterControllerScript : MonoBehaviour
 {
 	private Rigidbody2D rb;
-    //находится ли персонаж на земле или в прыжке?
 	private bool isGrounded = false;
-	//ссылка на компонент Transform объекта
-	//для определения соприкосновения с землей
 	public Transform groundCheck;
-	//радиус определения соприкосновения с землей
 	private float groundRadius = 0.2f;
-	//ссылка на слой, представляющий землю
 	public LayerMask whatIsGround;
-    //переменная для установки макс. скорости персонажа
     public float maxSpeed = 10f;
     public float jumpForce = 600;
     private float jumpTimeCounter;
     public float jumpTime;
     private bool isJumping;
-    //переменная для определения направления персонажа вправо/влево
     private bool isFacingRight = true;
-    //ссылка на компонент анимаций
     private Animator anim;
-
 
 	private int extraJumps;
 	public int extraJumpsValue;
 
-    /// <summary>
-    /// Начальная инициализация
-    /// </summary>
 	private void Start()
     {
 	extraJumps = extraJumpsValue;
@@ -39,61 +27,36 @@ public class CharacterControllerScript : MonoBehaviour
 	rb = GetComponent<Rigidbody2D>();
     }
 	
-    /// <summary>
-    /// Выполняем действия в методе FixedUpdate, т. к. в компоненте Animator персонажа
-    /// выставлено значение Animate Physics = true и анимация синхронизируется с расчетами физики
-    /// </summary>
 	private void FixedUpdate()
     {
-		//определяем, на земле ли персонаж
 		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround); 
-		//устанавливаем соответствующую переменную в аниматоре
 		anim.SetBool ("Ground", isGrounded);
-		//устанавливаем в аниматоре значение скорости взлета/падения
 		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
-        //если персонаж в прыжке - выход из метода, чтобы не выполнялись действия, связанные с бегом
-        /*if (!isGrounded)
-            return;*/
-        //используем Input.GetAxis для оси Х. метод возвращает значение оси в пределах от -1 до 1.
-        //при стандартных настройках проекта 
-        //-1 возвращается при нажатии на клавиатуре стрелки влево (или клавиши А),
-        //1 возвращается при нажатии на клавиатуре стрелки вправо (или клавиши D)
+        //if (!isGrounded)
+            //return;
         float move = Input.GetAxis("Horizontal");
 
-        //в компоненте анимаций изменяем значение параметра Speed на значение оси Х.
-        //приэтом нам нужен модуль значения
         anim.SetFloat("Speed", Mathf.Abs(move));
 
-        //обращаемся к компоненту персонажа RigidBody2D. задаем ему скорость по оси Х, 
-        //равную значению оси Х умноженное на значение макс. скорости
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-        //если нажали клавишу для перемещения вправо, а персонаж направлен влево
         if(move > 0 && !isFacingRight)
-            //отражаем персонажа вправо
             Flip();
-        //обратная ситуация. отражаем персонажа влево
         else if (move < 0 && isFacingRight)
             Flip();
     }
 
-    /// <summary>
-    /// Метод для смены направления движения персонажа и его зеркального отражения
-    /// </summary>
 	private void Update()
 	{
 		if (isGrounded == true)
 		{
 			extraJumps = extraJumpsValue;
 		}
-		//если персонаж на земле и нажат пробел...
 		if (extraJumps > 0 && Input.GetKeyDown (KeyCode.Space)) 
 		{
 			isJumping = true;
 			jumpTimeCounter = jumpTime;
-			//устанавливаем в аниматоре переменную в false
 			anim.SetBool("Ground", false);
-			//прикладываем силу вверх, чтобы персонаж подпрыгнул
             		rb.velocity = Vector2.up * jumpForce;
 			extraJumps--;				
 		} else if (extraJumps == 0 && Input.GetKeyDown (KeyCode.Space) && isGrounded == true)
@@ -116,13 +79,9 @@ public class CharacterControllerScript : MonoBehaviour
 	}
     private void Flip()
     {
-        //меняем направление движения персонажа
         isFacingRight = !isFacingRight;
-        //получаем размеры персонажа
         Vector3 theScale = transform.localScale;
-        //зеркально отражаем персонажа по оси Х
         theScale.x *= -1;
-        //задаем новый размер персонажа, равный старому, но зеркально отраженный
         transform.localScale = theScale;
     }
 }
